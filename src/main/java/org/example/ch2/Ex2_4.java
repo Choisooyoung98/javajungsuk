774f76a3ce209270b7384d03db4d85af3ea0a964
Choisooyoung98 <ekfms15815@gmail.com>
2023-02-18 22:09:29 +0900

    ch2-4 변수의 타입

---
+++ src/main/java/org/example/ch2/Ex2_4.java
@@ -0,0 +1,19 @@
+package org.example.ch2;
+
+/*
+1. 변수의 타입
+    변수의 타입은 저장할 값의 타입에 의해 결정된다.
+    저장할 값의 타입과 일치하는 타입으로 변수를 선언
+        char ch = '가';
+2. 값의 타입
+    문자 - char
+    숫자 - 정수 - byte, short, int, long
+            실수 - float, double
+    논리 - boolean
+
+*/
+public class Ex2_4 {
+    public static void main(String[] args) {
+
+    }
+}
