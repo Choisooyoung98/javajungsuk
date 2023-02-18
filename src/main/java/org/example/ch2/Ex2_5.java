f3e07e1a86915826a538c0528dba37b5922c13d6
Choisooyoung98 <ekfms15815@gmail.com>
2023-02-18 22:31:26 +0900

    ch2-5 상수와 리터럴

---
+++ src/main/java/org/example/ch2/Ex2_5.java
@@ -0,0 +1,14 @@
+package org.example.ch2;
+
+/*
+상수와 리터럴
+변수(variable) - 하나의 값을 저장하기 위한 공간
+상수(constant) - 한 번만 값을 저장 가능한 변수 ex) final int max = 100;
+리터널(literal) - 그 자체로 값을 의미하는 것(기존의 상수)
+*/
+public class Ex2_5 {
+    public static void main(String[] args) {
+        final int score = 100;
+        System.out.println(score);
+    }
+}
