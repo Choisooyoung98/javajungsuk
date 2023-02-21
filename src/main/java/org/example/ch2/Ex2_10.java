78a1a5a7e32dfefdb9cdcd5e4347c03deab38e71
Choisooyoung98 <ekfms15815@gmail.com>
2023-02-21 17:46:02 +0900

    2-10 기본형과 참조형

---
+++ src/main/java/org/example/ch2/Ex2_10.java
@@ -0,0 +1,16 @@
+package org.example.ch2;
+
+/*
+2-10 기본형과 참조형
+기본형
+    오직 8개(boolean, char, byte - 1, short - 2, int - 4, long - 8, float, double)
+    실제 값을 저장
+참조형
+    기본형을 제외한 나머지(String, System 등)
+    메모리 주소를 저장(32bit JVM - 4byte 또는 64bit JVM - 8byte)
+*/
+public class Ex2_10 {
+    public static void main(String[] args) {
+
+    }
+}
