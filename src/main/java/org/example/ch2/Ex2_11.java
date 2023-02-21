0f968716f9b0ee59ecce3e53a8c1d222f8f34af9
Choisooyoung98 <ekfms15815@gmail.com>
2023-02-21 18:27:27 +0900

    2-11 기본형 종류와 범위

---
+++ src/main/java/org/example/ch2/Ex2_11.java
@@ -0,0 +1,19 @@
+package org.example.ch2;
+
+/*
+2-11 기본형 종류와 범위
+논리형 - true 와 false 중 하나를 값으로 갖으며, 조건식과 논리적 계산에 사용
+문자형 - 문자를 저장하는데 사용되며, 변수 당 하나의 문자만 저장할 수 있다.
+정수형 - 정수 값을 저장하는데 사용된다. 주로 사용하는 것은 int와 long이며, byte는 이진 데이터를 다루는데 사용되며, short는 c언어와의 호환을 위해 추가되었다.
+실수형 - 실수 값을 저장하는데 사용된다. float와 double이 있다.
+논리형
+boolean - 1
+문자형
+char - 2
+정수형
+byte - 1, short - 2, int - 4, long - 8
+실수형
+float - 4, double - 8
+*/
+public class Ex2_11 {
+}
